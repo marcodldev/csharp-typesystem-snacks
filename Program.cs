d7b6336490273d51a2f808924f1549bbbfd8a762
marcodldev <marcodelisi2@gmail.com>
2023-03-16 16:02:30 +0100

    + snack 3

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿
 // snack1();
-snack2();
+// snack2();
 
+snack3();
 
+// CONDIZIONALI
 void snack1()
 {
     //Snack1.L’utenteinserisceduenumeriinsuccessione.
@@ -51,4 +53,29 @@ void snack2()
         Console.WriteLine($"la parola {parola2} è più lunga della parola {parola1}");
     }
 
+}
+
+
+// CICLO FOR
+
+void snack3()
+{
+    /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+    Il programma stampa la somma di tutti i numeri inseriti. */
+
+    int[] arraynumeri = new int[10];
+    int somma = 0;
+
+    for (int i = 0; i < arraynumeri.Length; i++) 
+    {
+        Console.WriteLine($"Inserisci il {i + 1}° numero: ");
+        arraynumeri[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    foreach (int numero in arraynumeri)
+    {
+        somma += numero;
+    }
+
+    Console.WriteLine($"La somma dei numeri è {somma}");
 }
